feb5a1ca8efd3deefb0361363f7001cd6ca03ded
jhon <jhonRawls@gmail.com>
2016-06-22 22:39:53 +0800

    first commit

---
+++ src/main/java/cn/ibadi/springtest/App.java
@@ -0,0 +1,13 @@
+package cn.ibadi.springtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
