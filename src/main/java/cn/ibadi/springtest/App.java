52da5b9c05ae38ff5def187a0a9101b776f1541f
jhonRawls <jhonRawls@gmail.com>
2016-07-28 22:17:36 +0800

    删除注释

--- src/main/java/cn/ibadi/springtest/App.java
+++ src/main/java/cn/ibadi/springtest/App.java
@@ -1,7 +1,6 @@
 package cn.ibadi.springtest;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
@@ -11,33 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App {
 	public static void main(String [] args){
-//		@SuppressWarnings("resource")
-		//ApplicationContext`
-//		AbstractApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
-//		HelloWorld obj=(HelloWorld)context.getBean("helloWorld");
-		
-//		XmlBeanFactory factory=new XmlBeanFactory(new ClassPathResource("Beans.xml"));
-//		HelloWorld obj=(HelloWorld)factory.getBean("helloWorld");
-		
-//		ApplicationContext context=new FileSystemXmlApplicationContext("E:/JavaCode/spring_learn/src/main/java/Beans.xml");
-//		HelloWorld obj=(HelloWorld)context.getBean("helloWorld");
-//		obj.getMessage();
-//		context.registerShutdownHook();
-//		
-//		Parent parent=(Parent)context.getBean("parentBean");
-//		
-//		parent.getMessage1();
-//		parent.getMessage2();
-//		
-//		Child child=(Child)context.getBean("childBean");
-//		child.getMessage1();
-//		child.getMessage2();
-//		child.getMessage3();
-
-		
 		ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
-		
-		
 	}
 	
 	
