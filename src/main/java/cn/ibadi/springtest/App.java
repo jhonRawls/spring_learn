e8b543805a71e16645229ed012437ce3e3541c54
jhonRawls <jhonRawls@gmail.com>
2016-07-31 13:09:58 +0800

    spring bean 注入

--- src/main/java/cn/ibadi/springtest/App.java
+++ src/main/java/cn/ibadi/springtest/App.java
@@ -3,15 +3,16 @@ package cn.ibadi.springtest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 /**
  * Hello world!
- *
  */
 public class App {
-	public static void main(String [] args){
-		ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
-	}
-	
-	
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        TextEditor textEditor=(TextEditor)context.getBean("textEditor");
+        textEditor.spellCheck();
+
+    }
+
+
 }
